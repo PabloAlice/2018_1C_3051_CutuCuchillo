0f0656da9fcb298644113a1dbe7f5a3ac173e2c9
TorricoJM <juant_lu@lagash.com>
2018-06-07 14:07:54 -0300

    floatmodifier

---
+++ TGC.Group/Model/FloatModifier.cs
@@ -0,0 +1,29 @@
+﻿
+
+using TGC.Core.Mathematica;
+
+namespace TGC.Group.Model
+{
+    public class FloatModifier
+    {
+        private float minValue, maxValue;
+        public float Modifier { get; set; }
+
+        public FloatModifier(float initialValue, float minValue, float maxValue)
+        {
+            Modifier = initialValue;
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public void Increment(float value)
+        {
+            Modifier = FastMath.Min(Modifier + value, maxValue);
+        }
+
+        public void Decrement(float value)
+        {
+            Modifier = FastMath.Max(Modifier - value, minValue);
+        }
+    }
+}
